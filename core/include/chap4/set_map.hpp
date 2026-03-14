bcafb5c21dfe6519da86a2b0d38dc914106a7f84
CemGurhan <cemg@live.co.uk>
2026-03-14 13:28:30 +0000

    hash

--- core/include/chap4/set_map.hpp
+++ core/include/chap4/set_map.hpp
@@ -1,6 +1,7 @@
 #include <map>
 #include<string>
 #include <boost/functional/hash.hpp>
+#include <unordered_set>
 
 void map_set_ordered() {
     auto mO = std::map<int, int>{}; // ordered map
@@ -29,4 +30,40 @@ void map_unordered() {
         boost::hash_combine(seed, person.age()); 
         return seed;
     };
+
+    // Note: we must have the hash and equality be decltype as lambdas are anonymous types,
+    // in order to instantiate templates we need types, decltype extracts the type out of the lambda.
+    using Set = std::unordered_set<person, decltype(person_hash), decltype(person_eq)>; 
+
+    auto persons = Set{100, person_hash, person_eq};
+}
+
+struct PersonHash {
+    size_t operator()(const person& p) const {
+        auto seed = size_t{0};
+        boost::hash_combine(seed, p.name()); 
+        boost::hash_combine(seed, p.age()); 
+        return seed;
+    }
+};
+
+struct PersonEquality {
+    bool operator()(const person& lhs, const person& rhs) const {
+        return lhs.name() == rhs.name() && lhs.age() == rhs.age();
+    }
+};
+
+
+void map_unordered_struct() {
+    // call operator structs can be passed in directly to template as they're objects.
+    std::unordered_set<person, PersonHash, PersonEquality> persons;
+
+    auto p = person{30, "Joey"};
+
+    persons.emplace(p);
+    
+    // key person val int
+    std::unordered_map<person, int, PersonHash, PersonEquality> personsMap;
+
+    personsMap[p] = 115;
 }
